a880eec9b985d3dc201751b9a199963895086395
Michał Ławinski <michrarr@gmail.com>
2025-08-16 10:06:51 +0200

    Upgrade to angular v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { PagesModule } from '@pages/pages.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http'
 import { ComponentsModule } from '@shared/components/components.module';
 
@@ -14,30 +14,24 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http);
 }
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    PagesModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    ComponentsModule,
-    TranslateModule.forRoot({
-      loader: {
-      provide: TranslateLoader,
-      useFactory: HttpLoaderFactory,
-      deps: [HttpClient]
-      }
-    }),
-  ],
-  providers: [
-    TranslateService
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        PagesModule,
+        BrowserAnimationsModule,
+        ComponentsModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })], providers: [
+        TranslateService,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {
 
   constructor(private translateService: TranslateService) {
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,7 +3,7 @@ import { SettlementsComponent } from './settlements/settlements.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { SettlementDialogComponent } from './settlements/settlement-dialog/settlement-dialog.component';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { SettlementsService } from '@services/settlement.service';
 import { SettlementSavingDialogComponent } from './settlements/settlement-saving-dialog/settlement-saving-dialog.component';
 
