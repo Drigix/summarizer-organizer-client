c4ef3203a1a6f89d0f8b30242af5cd511ef3a216
Michał Ławinski <michrarr@gmail.com>
2026-02-21 12:49:38 +0100

    Include another class in type of emit

--- src/app/entities/types/emit-types.ts
+++ src/app/entities/types/emit-types.ts
@@ -1,9 +1,10 @@
 import { Settlement } from "@entities/settlement.model";
 import { ButtonClickType } from "./button-click.types";
 import { PriceType } from "./price.types";
+import {SettlementSaving} from "@entities/settlement-saving.model";
 
 export type EmitSettlementPreviewType = {
   buttonClickType: ButtonClickType;
-  settlement: Settlement;
+  settlement: Settlement | SettlementSaving;
   priceType: PriceType
 };
