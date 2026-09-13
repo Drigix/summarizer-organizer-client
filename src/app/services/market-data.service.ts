5ec4846da7845f4d8b24f9d630ea80272a6eef86
Michał Ławinski <michrarr@gmail.com>
2026-09-13 21:08:03 +0200

    Add new endpoints to market proxy service

--- src/app/services/market-data.service.ts
+++ src/app/services/market-data.service.ts
@@ -11,11 +11,27 @@ export class MarketDataService {
    
     constructor(private http: HttpClient) { }
 
+    createStockCompany(stockCompany: StockCompany): Observable<StockCompany> {
+        return this.http.post<StockCompany>(`${this.resourceUrl}/stock-company`, stockCompany);
+    }
+
+    updateStockCompany(stockCompany: StockCompany): Observable<StockCompany> {
+        return this.http.put<StockCompany>(`${this.resourceUrl}/stock-company`, stockCompany);
+    }
+
+    deleteStockCompany(stockSymbol: string): Observable<void> {
+        return this.http.delete<void>(`${this.resourceUrl}/stock-company/${stockSymbol}`);
+    }
+
     getStockCompanies(): Observable<StockCompany[]> {
         return this.http.get<StockCompany[]>(`${this.resourceUrl}/stock-company/all`);
     }
 
     getStockPrices(symbol: string): Observable<any> {
         return this.http.get(`${this.resourceUrl}/stock-price/${symbol}`);
+    }
+
+    updateStockCompanyPrice(symbol: string): Observable<StockCompany> {
+        return this.http.put(`${this.resourceUrl}/stock-price/${symbol}`, {});
     }
 }
