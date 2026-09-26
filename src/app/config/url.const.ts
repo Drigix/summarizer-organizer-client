e3bc19cddb2b1ad7ddd066c7379dffbf66c6671d
Michał Ławinski <michrarr@gmail.com>
2026-09-26 09:43:17 +0200

    Set corrent url

--- src/app/config/url.const.ts
+++ src/app/config/url.const.ts
@@ -1,3 +1,3 @@
-// export const SERVER_URL = '/api/';
-export const SERVER_URL = 'http://localhost:3000/api/';
+export const SERVER_URL = '/api/';
+// export const SERVER_URL = 'http://localhost:3000/api/';
 
